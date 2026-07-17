afc178445cdd00ff5a870d853f0ccc00079eb292
Napoleone2 <121799593+Napoleone2@users.noreply.github.com>
2026-07-17 11:28:45 +0000

    date formatting

--- My Grand Strategy Game/time.lua
+++ My Grand Strategy Game/time.lua
@@ -1,12 +1,13 @@
 function loadtime()
     months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}
-
+    special_day_suffix = { "st", "nd", "rd" }
     time = 0
     time_speed = 1
     time_paused = true
     paused = "Paused"
     
     time_scale = 1
+
 end
 
 function timemgr()
@@ -25,6 +26,17 @@ function timemgr()
     year = math.floor(month / 12) + 684          -- Start at Year 684
     month_of_year = math.floor(month % 12) + 1
     day_of_month = math.floor(days % 30) + 1    -- Day 1 to Day 28
+    
+    function add_suffix()
+        suffix = "th"
+        if day_of_month ~= 11 and day_of_month ~= 12 and day_of_month ~= 13 and day_of_month % 10 >= 1 and day_of_month % 10 <= 3 then
+            suffix = special_day_suffix[day_of_month % 10]
+        end
+    end
+    add_suffix()
+
+    date = day_of_month .. suffix .. " of " .. months[month_of_year] .. " " .. year .. " | " .. paused
+
 end
 
 function love.keypressed(key)
